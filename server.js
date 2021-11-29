68df949484f4b7c4ea635c4732456d8613bda146
Dan P <me@dipanp.com>
2021-11-29 12:03:03 -0600

    Comment Code Smell Refactoring for Server.JS

--- server.js
+++ server.js
@@ -2,22 +2,25 @@
  * 
  */
 
-var express = require('express'); // express framework for server
-var handlebars = require('express-handlebars').create({defaultLayout:'main'}); // templating engine
-var session = require('express-session'); // save user data across a session, realistically don't need cookies
-var cors = require('cors'); // enable data from outside this domain
-var env = require('dotenv').config(); // enable environment variables from process.env file
-var axios = require('axios'); // enable POST requests using Axios library
-const e = require('express');
+// Frameworks
+var express = require('express'); // Use: server framework
+var handlebars = require('express-handlebars').create({defaultLayout:'main'}); // Use: templating engine
+var session = require('express-session'); // Use: save user data across a session, realistically don't need cookies
+var cors = require('cors'); // Use: enable data from outside this domain
+var env = require('dotenv').config(); // Use: enable environment variables from process.env file
+var axios = require('axios'); // Use: enable POST requests using Axios library
 
-var app = express(); // create express instance called app
+
+// Setup Framework
+var app = express();
 app.use(express.json()); // set app to recognize incoming objects as JSON
-app.use(express.urlencoded({extended: true})); // ##TODO - this might be default now... not entirely sure if necessary
-app.use(cors()); // ##TODO
-app.use('/public',express.static(__dirname + '/public')); // set public folder as the web root
-console.log(__dirname + '/public');
+app.use(express.urlencoded({extended: true}));
 
-app.set('port', process.env.PORT); // set app to run on port 4077
+app.use(cors());
+
+app.use('/public',express.static(__dirname + '/public'));
+
+app.set('port', process.env.PORT);
 app.set('authClient', {
 	  client_url: process.env.CLIENT_URL,
 	  client_id: process.env.SPOTIFY_CLIENT_ID,
@@ -25,23 +28,29 @@ app.set('authClient', {
 	}
 );
 
-// set app to use sessions
 app.use(session({
 	secret: process.env.SESSION_SECRET,
 	resave: true,
 	saveUninitialized: true
 }));
 
-//initialize and set app to use handlebars templating engine for views
+// initialize and set app to use handlebars templating engine for views
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+/**
+ * Page Specific Module Imports
+ */
 app.use('/login', require('./login.js'));
 app.use('/profile', require('./profile.js'));
 app.use('/playlist', require('./playlist.js'));
 app.use('/about', require('./about.js'));
 
-app.get('/', function(req, res){
+/**
+ * Home Page
+ * Render login page or redirect to profile if already logged in (via session check)
+ */
+app.get(['/', '/home'], function(req, res){
 	var context = {};
 	if(req.session.profile) {
 		res.redirect('/profile');
@@ -51,29 +60,26 @@ app.get('/', function(req, res){
 	}
 });
 
-app.get('/home', function(req, res){
-	var context = {};
-	if(req.session.profile) {
-		res.redirect('/profile');
-	}
-	else {
-		res.render('home', context);
-	}
-});
-
+/**
+ * 404 Error Page
+ */
 app.use(function(req,res){
 	res.status(404);
 	res.render('404');
 });
 
+/**
+ * 500 Error Page
+ */
 app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
 });
 
-// ##TODO
+/**
+ * Launch Server on 
+ */
 app.listen(app.get('port'), function(){
-	//console.log(app);
-	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+	console.log('Express started on ' + app.get('authClient').CLIENT_URL + ':' + app.get('port') + '; press Ctrl-C to terminate.');
 });
